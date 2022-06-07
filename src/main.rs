a82b4a41f2ae11cf230130d93e8499a225748b80
Jill Please <jillplspls@gmail.com>
2022-06-07 16:18:54 +0200

    switch to tera templating

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod posts;
-
-use rocket::fs::{FileServer, relative};
 use posts::page;
-
+use rocket::fs::{relative, FileServer};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from(relative!("static"))).mount("/wiki", routes![page])
+    rocket::build()
+        .mount("/", FileServer::from(relative!("static")))
+        .mount("/wiki", routes![page])
 }
--- src/posts.rs
+++ src/posts.rs
@@ -1,30 +1,23 @@
-use askama::Template;
+extern crate tera;
+
 use rocket::response::content;
-use std::path::Path;
 use std::fs::read_to_string;
-
-#[derive(Template)]
-#[template(path = "post.html")]
-struct PostTemplate<'a> {
-    title: &'a str,
-    content: &'a str,
-}
-
-#[derive(Template)]
-#[template(path = "page_not_found.html")]
-struct PageNotFoundTemplate<'a> {
-    title: &'a str,
-}
+use std::path::Path;
+use tera::{Context, Tera};
 
 #[get("/<title>")]
 pub fn page(title: &str) -> content::RawHtml<String> {
+    let tera = Tera::new("templates/**/*.html").unwrap();
+    let mut context = Context::new();
+    context.insert("title", title);
+
     let path = format!("data/pages/{}", title);
     let content_path = Path::new(&path);
-    if ! content_path.exists() {
-        content::RawHtml( (PageNotFoundTemplate { title : title }).render().unwrap() )
+    if !content_path.exists() {
+        content::RawHtml(tera.render("page_not_found.html", &context).unwrap())
     } else {
-        let content = read_to_string(content_path).unwrap(); 
-        let page = PostTemplate { title : title, content : &content };
-        content::RawHtml(page.render().unwrap())
+        let content = read_to_string(content_path).unwrap();
+        context.insert("content", &content);
+        content::RawHtml(tera.render("post.html", &context).unwrap())
     }
 }
